27887ca590730e86762eaf1ec9eef0ba88b186a5
zakaria elaroui <zakariaelaroui122@gmail.com>
2024-08-27 12:04:42 +0100

    to be continued

--- src/main/java/ma/ram/commercialapp/controllers/ClientController.java
+++ src/main/java/ma/ram/commercialapp/controllers/ClientController.java
@@ -1,8 +1,6 @@
 package ma.ram.commercialapp.controllers;
 
 import ma.ram.commercialapp.entities.Client;
-import ma.ram.commercialapp.entities.Commercial;
-import ma.ram.commercialapp.repositories.CategoryRepository;
 import ma.ram.commercialapp.repositories.CommercialRepository;
 import ma.ram.commercialapp.services.ClientServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
-
-
 @RestController
 @RequestMapping("/api/client")
 public class ClientController {
@@ -27,7 +22,6 @@ public class ClientController {
         this.clientService = clientService;
         this.commercialRepository = commercialRepository;
     }
-
     @GetMapping
     public ResponseEntity<Page<Client>> clientList(@AuthenticationPrincipal UserDetails userDetails,
                                                    @RequestParam(defaultValue = "0") int page,
@@ -36,18 +30,15 @@ public class ClientController {
         Long IdCommercial=commercialRepository.findByUsername(userDetails.getUsername()).getId();
         return ResponseEntity.ok(clientService.viewAllCommercialClients(IdCommercial,pageable));
     }
-
     @PostMapping
     public ResponseEntity<Client> addClient(@RequestBody Client client,@AuthenticationPrincipal  UserDetails userDetails){
         Long idCommercial=commercialRepository.findByUsername(userDetails.getUsername()).getId();
         return ResponseEntity.created(URI.create("/api/client"+client.getId())).body(clientService.addClient(client,idCommercial));
     }
-
     @PostMapping("/{id}")
     public ResponseEntity<Client> showClientById(@PathVariable Long id){
         return ResponseEntity.ok(clientService.viewClient(id));
     }
-
     @DeleteMapping("/delete")
     public void deleteClient(@RequestParam Long id){
         clientService.deleteClient(id);
@@ -56,7 +47,6 @@ public class ClientController {
     public ResponseEntity<Client> editClient(@RequestBody Client client){
          return ResponseEntity.ok(clientService.editClient(client));
     }
-
     @PostMapping("addToCategory")
     public void addClientToCategory(@RequestParam Long idCategory,@AuthenticationPrincipal  UserDetails userDetails,@RequestParam Long idClient){
         clientService.addClientToCategory(idCategory,idClient,commercialRepository.findByUsername(userDetails.getUsername()).getId());
